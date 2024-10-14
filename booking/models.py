135351044106e86c0961b0bbaa852142daa965a6
Sam Green <sam.green7777@yahoo.com>
2024-10-14 20:49:42 +0000

    update models in booking app

--- booking/models.py
+++ booking/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Reservation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
-    time = models.TimeField()
-    number_of_guests = models.IntegerField()
+
+class Table(models.Model):
+    capacity = models.IntegerField()
+
+    class Meta:
+        ordering = ('-capacity',)
 
     def __str__(self):
-        return f"Reservation for {self.user.username} on {self.date} at {self.time}"
+        return f"Table Capacity: {self.capacity}"
 
 
+class Booking(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='bookings')
+    table = models.ForeignKey(
+        Table, on_delete=models.CASCADE, related_name='bookings')
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
 
-# Create your models here.
+    def __str__(self):
+        return f"Booking for {self.user.username.capitalize()} on {self.date} at {self.time} for {self.guests} guests"
