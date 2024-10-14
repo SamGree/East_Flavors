0ee68c638ff820651e92180bdd1dab887374d698
Sam Green <sam.green7777@yahoo.com>
2024-10-14 21:00:20 +0000

    create url file in booking app and add URL patterns for user authentication and booking features

---
+++ booking/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # auth
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
+    # bookings
+    path('book-table/', views.book_table, name='book-table'),
+    path('user-bookings/', views.user_bookings, name='user-bookings'),
+    path('cancel-booking/<int:id>/', views.cancel_booking, name='cancel-booking'),
+]
