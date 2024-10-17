152e58c43d32e34d465f80432e92cfcfa34e3672
Sam Green <sam.green7777@yahoo.com>
2024-10-17 18:28:32 +0000

    installed all over again

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-samgree-eastflavors-li2crwtl6ah.ws-eu116.gitpod.io',
+ALLOWED_HOSTS = ['8000-samgree-eastflavors-45ndkqs7v56.ws.codeinstitute-ide.net',
 '.herokuapp.com']
 
 
@@ -105,6 +105,11 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
