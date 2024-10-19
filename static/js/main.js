40ad05e10d5e60b5cc5e26bc2ccc3d002b504f00
Sam Green <sam.green7777@yahoo.com>
2024-10-19 08:36:59 +0000

    add js code for active nav bar

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,16 @@ hamburger.addEventListener("click", () => {
 
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone; // e.g. "America/New_York"
 document.cookie = "django_timezone=" + timezone;
+
+// Add logic for active navbar link
+const currentPath = window.location.pathname;
+
+// Get all links in the navigation menu
+const links = document.querySelectorAll('.nav-links a');
+
+// Loop through links and add the 'active' class to the one that matches the current URL
+links.forEach(link => {
+  if (link.getAttribute('href') === currentPath) {
+    link.classList.add('active');
+  }
+});
