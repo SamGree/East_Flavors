0b314420575e4aff8ac53d427fa9b6dcab26ee12
Sam Green <sam.green7777@yahoo.com>
2024-10-20 12:03:48 +0000

    delete MEDIA_URL

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -31,6 +31,3 @@ urlpatterns = [
     path('booking/', include('booking.urls')),   
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
